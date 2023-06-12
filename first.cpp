acf01d1fe3959571268b14992ea8b66596b0aea2
SubhamPan <118278323+SubhamPan@users.noreply.github.com>
2023-06-12 16:10:21 +0530

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -121,6 +121,20 @@ if(x>1) {
 
 //============================================================
 
+bool isprime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+//============================================================
+
 //all factors list
 //e.g.
 //int a = 36
@@ -144,36 +158,4 @@ vector<int> allfactorslist(int x) {
 
 
 //============================================================
-
-class DSU {
-public:
-    int n;
-    //p is parents vector, sz is size vector
-    vector<int> p, sz;
-
-    //constructor initializing DSU object with given number of elements '_n'
-    DSU(int _n) : n(_n) {
-        p.resize(n);
-        iota(p.begin(), p.end(), 0); //p vector is now: 0 1 2 3 4 ... n-1
-        //so effectively intializing each element as its own parent
-        sz.resize(n, 1);
-    }
-
-    int find(int x) {
-        if(x==p[x]) return x;
-        return p[x] = find(p[x]);
-    }
-
-    void unite(int a, int b) {
-        int c = find(a);
-        int d = find(b);
-        if(d!=c) {
-            if(sz[c] < sz[d]) swap(c,d);
-
-            p[d] = c;
-            sz[c] += sz[d];
-        }
-    }
-
-};
 
