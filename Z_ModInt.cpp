52f5baf1b7357228c84e4c88426eff0c1e268c8e
SubhamPan <118278323+SubhamPan@users.noreply.github.com>
2023-06-12 15:52:50 +0530

    Create Z_ModInt.cpp

---
+++ Z_ModInt.cpp
@@ -0,0 +1,145 @@
+//==================================================================
+
+//modular arithmetic for MOD type questions code below:
+
+
+
+constexpr int P = 1000000007;
+// assume -P <= x < 2P
+int norm(int x) {
+    if (x < 0) {
+        x += P;
+    }
+    if (x >= P) {
+        x -= P;
+    }
+    return x;
+}
+template<class T>
+T power(T a, ll b) {
+    T res = 1;
+    for (; b; b /= 2, a *= a) {
+        if (b % 2) {
+            res *= a;
+        }
+    }
+    return res;
+}
+struct Z {
+    int x;
+    Z(int x = 0) : x(norm(x)) {}
+    Z(ll x) : x(norm(x % P)) {}
+    int val() const {
+        return x;
+    }
+    Z operator-() const {
+        return Z(norm(P - x));
+    }
+    Z inv() const {
+        assert(x != 0);
+        return power(*this, P - 2);
+    }
+    Z &operator*=(const Z &rhs) {
+        x = ll(x) * rhs.x % P;
+        return *this;
+    }
+    Z &operator+=(const Z &rhs) {
+        x = norm(x + rhs.x);
+        return *this;
+    }
+    Z &operator-=(const Z &rhs) {
+        x = norm(x - rhs.x);
+        return *this;
+    }
+    Z &operator/=(const Z &rhs) {
+        return *this *= rhs.inv();
+    }
+    Z &operator%=(const Z &rhs) {
+        x = x % rhs.x;
+        return *this;
+    }
+    friend Z operator*(const Z &lhs, const Z &rhs) {
+        Z res = lhs;
+        res *= rhs;
+        return res;
+    }
+    friend Z operator+(const Z &lhs, const Z &rhs) {
+        Z res = lhs;
+        res += rhs;
+        return res;
+    }
+    friend Z operator-(const Z &lhs, const Z &rhs) {
+        Z res = lhs;
+        res -= rhs;
+        return res;
+    }
+    friend Z operator/(const Z &lhs, const Z &rhs) {
+        Z res = lhs;
+        res /= rhs;
+        return res;
+    }
+    friend Z operator%(const Z &lhs, const Z &rhs) {
+        Z res = lhs;
+        res %= rhs;
+        return res;
+    }
+    friend bool operator==(const Z &lhs, const Z &rhs) {
+        if(lhs.x == rhs.x) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    friend bool operator!=(const Z &lhs, const Z &rhs) {
+        if(lhs.x != rhs.x) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    friend bool operator<(const Z &lhs, const Z &rhs) {
+        if(lhs.x < rhs.x) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    friend bool operator>(const Z &lhs, const Z &rhs) {
+        if(lhs.x > rhs.x) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    friend istream &operator>>(istream &is, Z &a) {
+        ll v;
+        is >> v;
+        a = Z(v);
+        return is;
+    }
+    friend ostream &operator<<(ostream &os, const Z &a) {
+        return os << a.val();
+    }
+
+};
+
+//Z ans;
+// or
+//Z fact[100005];
+//u get the point ig, Z is a new datatype like int.
+//if u do Z%2, be sure to change P to 1000000000.
+//===========================================================
+
+Z fac[100005];
+
+Z NcRmodPFermat(int n, int r) {
+    if(n<r) {return 0;}
+    if(r==0) {return 1;}
+
+    //fill factorial array
+
+    return fac[n]/(fac[r]*fac[n-r]);
+}
+
+
+//===========================================================
