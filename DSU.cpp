7edefb667d03075f9b507a6cbb101700b44e3895
SubhamPan <118278323+SubhamPan@users.noreply.github.com>
2023-06-12 15:56:06 +0530

    Create DSU.cpp

---
+++ DSU.cpp
@@ -0,0 +1,31 @@
+class DSU {
+public:
+    int n;
+    //p is parents vector, sz is size vector
+    vector<int> p, sz;
+
+    //constructor initializing DSU object with given number of elements '_n'
+    DSU(int _n) : n(_n) {
+        p.resize(n);
+        iota(p.begin(), p.end(), 0); //p vector is now: 0 1 2 3 4 ... n-1
+        //so effectively intializing each element as its own parent
+        sz.resize(n, 1);
+    }
+
+    int find(int x) {
+        if(x==p[x]) return x;
+        return p[x] = find(p[x]);
+    }
+
+    void unite(int a, int b) {
+        int c = find(a);
+        int d = find(b);
+        if(d!=c) {
+            if(sz[c] < sz[d]) swap(c,d);
+
+            p[d] = c;
+            sz[c] += sz[d];
+        }
+    }
+
+};
