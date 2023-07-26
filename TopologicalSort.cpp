7e5c5f4e112884e855416ddb8f3873151f1fcecb
SubhamPan <118278323+SubhamPan@users.noreply.github.com>
2023-07-26 11:00:14 +0530

    Create TopologicalSort.cpp

---
+++ TopologicalSort.cpp
@@ -0,0 +1,32 @@
+//dfs based topological sort
+vector<int> toposort(vector<vector<int>> &adj) {
+    int V = adj.size();
+    stack<int> Stack;
+    vector<bool> visited(V, false);
+
+    //dfs based topological sort
+    function<void(int)> toposort = [&](int v) -> void {
+        visited[v] = true;
+        for(auto i : adj[v]) {
+            if(visited[i]==false) {
+                toposort(i);
+            }
+        }
+
+        Stack.push(v);
+    };
+
+    for(int i = 0; i<V; i++) {
+        if(visited[i]==false) toposort(i);
+    }
+
+    vector<int> ans;
+    while(Stack.empty()==false) {
+        ans.push_back(Stack.top());
+        Stack.pop();
+    }
+
+    return ans;
+}
+
+//=====================================================================================
