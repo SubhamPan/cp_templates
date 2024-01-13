6c25fcb29eee8631c8db11ae0c163e8fd73f9e20
Subham Pan <118278323+SubhamPan@users.noreply.github.com>
2024-01-13 09:34:11 +0530

    Update LazySegTree.cpp

--- LazySegTree.cpp
+++ LazySegTree.cpp
@@ -1,3 +1,93 @@
+// tageter2004's lazy segtree template:
+// lsegtree<ll> sg(array)
+template<typename T>
+class lsegtree{
+	public:
+	struct node{
+		int val; int cnt;
+		int lazy = -1; // contents
+		void merge(const node& l,const node& r){ 
+			lazy = -1;
+			cnt = l.cnt + r.cnt;
+			val = l.val + r.val; 
+		};
+		void apply(){
+			val = lazy*cnt;
+			lazy = -1;
+		};
+	};
+	int base = 1;
+	vector<node> tree;
+	lsegtree(vector<T> a){
+		int _n = len(a);
+		while(base < _n) base*=2;
+		tree.resize(2*base);
+		for(int i = 0; i < _n; i++) {
+			tree[base+i] = node{a[i],1};    // leaf values
+		}
+		for(int i = _n; i < base; i++) {
+			tree[base+i] = node{0,1};    // default leaf values
+		}
+		for(int i = base-1;i>=1;i--) {
+			tree[i].merge(tree[2*i],tree[2*i + 1]);
+		}
+	};
+	// update in [q_l,q_r]
+	void update(int id, int l, int r, int q_l, int q_r, int sett){
+		if(tree[id].lazy != -1){ // lazy condition
+			l==r ? :tree[2*id].lazy = tree[id].lazy;
+			l==r ? :tree[2*id + 1].lazy = tree[id].lazy;
+			tree[id].apply();
+		}
+		if(l > q_r or r < q_l) {
+			return;
+		}
+		if(q_l <= l and q_r >= r) {
+			l==r ? :tree[2*id].lazy = sett ;
+			l==r ? :tree[2*id + 1].lazy = sett;
+			tree[id].val = sett*tree[id].cnt; // normal updates
+			return;
+		}
+		int m = (l + r)/2;
+		update(2*id, l, m, q_l, q_r, sett);
+		update(2*id + 1, m + 1, r, q_l, q_r, sett);
+		tree[id].merge(tree[2*id],tree[2*id + 1]);
+	};
+	// get from [q_l,q_r]
+	node get(int id , int l, int r, int q_l, int q_r){
+		if(tree[id].lazy != -1){ // lazy condition
+			l==r ? :tree[2*id].lazy = tree[id].lazy;
+			l==r ? :tree[2*id + 1].lazy = tree[id].lazy;
+			tree[id].apply();
+		}
+		if(q_l <= l and q_r >= r) {
+			return tree[id];
+		}
+		int m = (l + r)/2;
+		if(m + 1 > q_r)
+			return get(2*id, l, m, q_l, q_r);
+		else if(m < q_l)
+			return get(2*id + 1, m+1, r, q_l, q_r);
+		node newnode;
+		newnode.merge(get(2*id, l, m, q_l, q_r),get(2*id + 1, m+1, r, q_l, q_r));
+		return newnode;
+	};
+	void update(int q_l, int q_r, int sett){
+		update(1, 0, base-1, q_l, q_r, sett);
+	}
+	node get(int q_l, int q_r){
+		return get(1, 0, base-1, q_l, q_r);
+	}
+};
+
+
+
+
+//======================================================================================
+
+
+
+
  
 template<class T, class U>
 // T -> node, U->update.
