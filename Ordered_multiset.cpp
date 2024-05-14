dfb7da3e0bec21bbc86ec8ccd9f69eea11dc32fd
Subham Pan <118278323+SubhamPan@users.noreply.github.com>
2024-05-14 21:39:40 +0530

    Create Ordered_multiset.cpp

---
+++ Ordered_multiset.cpp
@@ -0,0 +1,33 @@
+// example usage: https://atcoder.jp/contests/abc353/submissions/53331960
+
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+template <typename T> struct ordered_multiset {
+    ordered_set<pair<T, int>> ost;
+    map<T, int> freq;
+
+    void insert(T val) {
+        ost.insert(make_pair(val, ++freq[val]));
+    }
+
+    void erase(T val) {
+        ost.erase(make_pair(val, freq[val]--));
+    }
+
+    int order_of_key(T val) {
+        return ost.order_of_key(make_pair(val, -1));
+    }
+
+    T find_by_order(T val) {
+        return ost.find_by_order(val)->first;
+    }
+
+    int size() {
+        return ost.size();
+    }
+
+    bool empty() {
+        return ost.empty();
+    }
+};
+
