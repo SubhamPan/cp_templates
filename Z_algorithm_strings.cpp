ea5f9494eb0facfbb8b5a0830c2bf4dba5b09736
Subham Pan <118278323+SubhamPan@users.noreply.github.com>
2024-05-20 15:49:52 +0530

    Update Z_algorithm_strings.cpp

--- Z_algorithm_strings.cpp
+++ Z_algorithm_strings.cpp
@@ -1,19 +1,24 @@
- 
-vector<int> z_function(const string& s) {
-	const int n = s.size();
-	vector<int> z(n);
-	int opt = 0;
-	for (int i = 1; i < n; ++i) {
-		if (i < opt + z[opt]) {
-			z[i] = min(z[i - opt], opt + z[opt] - i);
-		}
-		while (i + z[i] < n && s[i + z[i]] == s[z[i]]) {
-			++z[i];
-		}
-		if (opt + z[opt] < i + z[i]) {
-			opt = i;
-		}
-	}
-	z[0] = n;
-	return z;
+ /* z[i] = the greatest number of characters starting from the position 'i' that coincide with the first characters of s (string). */
+vector<int> z_function(string s) {
+    int n = s.size();
+    vector<int> z(n);
+    int l = 0, r = 0;
+    for(int i = 1; i < n; i++) {
+        // optimization case:
+        if(i < r) {
+            z[i] = min(r - i, z[i - l]);
+        }
+        // trivial case:
+        while(i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            z[i]++;
+        }
+
+        if(i + z[i] > r) {
+            l = i;
+            r = i + z[i];
+        }
+
+    }
+    z[0] = n;
+    return z;
 }
