91876f950fa895ff0a834e4faa96c58efd42861c
Subham Pan <118278323+SubhamPan@users.noreply.github.com>
2024-05-21 18:15:28 +0530

    Update Cycliccheck.cpp

--- Cycliccheck.cpp
+++ Cycliccheck.cpp
@@ -1,3 +1,4 @@
+// shadow's version:
 //This detects cycle in a ***directed graph***
 bool isCyclic(vector<vector<int>> &adj){
 	int V = adj.size();
@@ -24,3 +25,35 @@ bool isCyclic(vector<vector<int>> &adj){
 	}
 	return false;
 }
+
+
+
+// AshishGup's version: --------------------------------------------------------------------
+// put this in void solve(). Remember to change n+m to n or graph's size (number of nodes) wherever required.
+    // =================================================================================
+    // for directed graphs:
+    vector<ll> vis(n + m, 0);
+    // graph vector<vector<int>> g(n + m).
+    function<bool(int)> findLoop = [&](int v) -> bool {
+        if(vis[v] == 1) {return true;}
+        if(vis[v] == 2) {return false;}
+ 
+        vis[v] = 1;
+        for(auto& x : g[v]) {
+            if(findLoop(x)) {return true;}
+        }
+ 
+        vis[v] = 2; // i am done exploring for cycles/loops with this node. signing off.
+        return false;
+    };
+ 
+    // --------------------------------------------------------
+ 
+    auto checkLoop = [&]() -> bool {
+        fill(vis.begin(), vis.end(), 0LL);
+        for(ll i = 0; i < n + m; i++) { // there are n+m nodes in this graph (might want to change it to n)
+            if(vis[i] == 0 && findLoop(i)) {return true;}
+        }
+        return false;
+    };
+    
