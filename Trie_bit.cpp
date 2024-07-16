9d1eddd7fa5b751c4ad915ac970f8d499e13d32b
Subham Pan <118278323+SubhamPan@users.noreply.github.com>
2024-07-16 14:05:33 +0530

    Create Trie_bit.cpp

---
+++ Trie_bit.cpp
@@ -0,0 +1,122 @@
+
+struct trie {
+      static const ll B = 30;
+      struct node {
+            // bool ends; // unnecssary in bit trie
+            ll st_cnt;
+            array<node*, 2> next;
+
+            node() {
+                  for(ll i = 0; i < 2; i++) {
+                        next[i] = NULL;
+                  }
+                  st_cnt = 0;
+            }
+      };
+
+      node *head = NULL;
+      trie() {
+            head = new node();
+      }
+
+      void insert(ll val) {
+
+            node *curr = head;
+
+            
+            for(ll i = B; i >= 0; i--) {
+                  curr->st_cnt++;
+
+                  ll bit = val >> i & 1;
+
+                  if(curr->next[bit] == NULL) {
+                        curr->next[bit] = new node();
+                  }
+
+                  curr = curr->next[bit];
+                  // ans += curr->st_cnt;
+            }
+
+            curr->st_cnt++;
+      }
+
+      ll query(ll x, ll k) { 
+      // number of values s.t. val ^ x < k
+            node* curr = head;
+            ll ans = 0;
+            
+            for(ll i = B; i >= 0; i--) {
+                  if(curr == NULL) {break;}
+
+                  ll x_bit = x >> i & 1;
+                  ll k_bit = k >> i & 1;
+
+                  if(k_bit == 1) {
+                        if(curr->next[x_bit] != NULL) {
+                              ans += curr->next[x_bit]->st_cnt;
+                        }
+                        curr = curr->next[x_bit ^ 1];
+                  } else {
+                        // k == 0; 
+                        // so we can only choose those vals
+                        // .. that has to be same bit as x.
+                        curr = curr->next[x_bit];
+                  }
+            }
+
+            return ans;
+
+      }
+
+      ll get_max(ll x) {
+      // returns maximum of val ^ x
+            node* curr = head;
+            ll ans = 0;
+            for(ll i = B; i >= 0; i--) {
+                  ll x_bit = x >> i & 1;
+                  if(curr->next[x_bit ^ 1]) {
+                        curr = curr->next[x_bit ^ 1];
+                        ans <<= 1;
+                        ans++;
+                  } else {
+                        curr = curr->next[x_bit];
+                        ans <<= 1;
+                  }
+            }
+            return ans;
+      }
+
+      ll get_min(ll x) {
+      // returns minimum of val ^ x
+            node* curr = head;
+            ll ans = 0;
+            for(ll i = B; i >= 0; i--) {
+                  ll x_bit = x >> i & 1;
+                  if(curr->next[x_bit]) {
+                        curr = curr->next[x_bit];
+                        ans <<= 1;
+                  } else {
+                        curr = curr->next[x_bit ^ 1];
+                        ans <<= 1;
+                        ans++;
+                  }
+            }
+            return ans;
+      }
+
+      void delt(node* curr) {
+            if(curr == NULL) {return;}
+            for(ll i = 0; i < 2; i++) {
+                  delt(curr->next[i]);
+            }
+            delete curr;
+      }
+
+      ~trie() {
+            delt(head);
+      }
+};
+// trie T;
+// T.insert(x);
+
+// https://www.spoj.com/status/SUBXOR,dabest/
