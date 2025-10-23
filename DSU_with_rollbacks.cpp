fc7fe11b0f9f449ff7b6a52ef8801084e81f26aa
Subham Pan <118278323+SubhamPan@users.noreply.github.com>
2025-10-23 19:50:01 +0530

    kactl format added at the top
    yet to change tab-space to 4.

--- DSU_with_rollbacks.cpp
+++ DSU_with_rollbacks.cpp
@@ -1,6 +1,39 @@
 class DSUwithRollback {
   private:
       vector<int> p, sz;
+      vector<pair<int &, int>> history;
+  public:
+      DSUwithRollback(int n) : p(n), sz(n, 1) { iota(p.begin(), p.end(), 0); }
+      int find(int x) { return x == p[x] ? x : find(p[x]); }
+      bool merge(int a, int b) {
+            a = find(a); b = find(b);
+            if (a == b) { return false; }
+            if (sz[a] < sz[b]) { swap(a, b); }
+            history.push_back({sz[a], sz[a]});
+            history.push_back({p[b], p[b]});
+            p[b] = a; sz[a] += sz[b];
+            return true;
+      }
+      int snapshot() { return history.size(); }
+      void rollback(int until) {
+            while (snapshot() > until) {
+                  history.back().first = history.back().second;
+                  history.pop_back();
+            }
+      }
+      bool isSame(int x, int y) {return find(x)==find(y);}  
+      int getSize(int x) {return sz[find(x)];}
+};
+
+// https://codeforces.com/contest/1681/submission/159596854
+// https://codeforces.com/contest/1681/submission/265639005 -> [my submission for the saame question]
+
+
+
+// ==============================================================================================================================
+class DSUwithRollback {
+  private:
+      vector<int> p, sz;
       // stores previous unites
       vector<pair<int &, int>> history;
  
